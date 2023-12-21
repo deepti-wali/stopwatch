8e49e67db506e2ef5891ad648d52ae8ef67cf8b3
Deepti <134826480+deepti-wali@users.noreply.github.com>
2023-12-21 15:24:35 +0530

    first commit

---
+++ src/Stopwatch.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from "react";
+
+const Stopwatch = () => {
+  const [seconds, setSeconds] = useState(0);
+  const [isActive, setIsActive] = useState(false);
+
+  useEffect(() => {
+    let interval = null;
+    if (isActive) {
+      interval = setInterval(() => {
+        setSeconds((prevSeconds) => prevSeconds + 1);
+      }, 1000);
+    } else if (!isActive && seconds !== 0) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [isActive, seconds]);
+
+  const formatTime = (timeInSeconds) => {
+    const minutes = Math.floor(timeInSeconds / 60);
+    const remainingSeconds = timeInSeconds % 60;
+
+    const formattedMinutes = String(minutes).padStart(1, "0");
+    const formattedSeconds = String(remainingSeconds).padStart(2, "0");
+
+    return `${formattedMinutes}:${formattedSeconds}`;
+  };
+
+  const handleStartStop = () => {
+    setIsActive(!isActive);
+  };
+
+  const handleReset = () => {
+    setIsActive(false);
+    setSeconds(0);
+  };
+
+  return (
+    <div>
+      <p>Time: &nbsp; {formatTime(seconds)}</p>
+      <button onClick={handleStartStop}>{isActive ? "Stop" : "Start"}</button>
+      <button onClick={handleReset}>Reset</button>
+    </div>
+  );
+};
+
+export default Stopwatch;
